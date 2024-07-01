32641003fbc1db5fb8f5dc7a1d93c60e52bd6c77
Yasen Georgiev <yasen@tavex.eu>
2024-07-02 01:33:57 +0300

    Add labels for absence types.

--- src/main/java/bg/nbuteam4/myschool/enums/AbsenceType.java
+++ src/main/java/bg/nbuteam4/myschool/enums/AbsenceType.java
@@ -2,18 +2,24 @@ package bg.nbuteam4.myschool.enums;
 
 public enum AbsenceType {
 
-    SICK(1), // болест
-    FAMILY_REASONS(2), // домашни причини
-    LATE(3), // закъснение
-    ABSENT(9); // отсъствие
+    SICK(1, "Болест"), // болест
+    FAMILY_REASONS(2, "Домашни причини"), // домашни причини
+    LATE(3, "Закъснение"), // закъснение
+    ABSENT(9, "Отсъствие"); // отсъствие
 
     private final int value;
+    private final String label;
 
-    AbsenceType(int value) {
+    AbsenceType(int value, String label) {
         this.value = value;
+        this.label = label;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public String getLabel() {
+        return label;
     }
 }
