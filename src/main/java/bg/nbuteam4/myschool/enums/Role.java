fd7941e2ae2d95fe00f85713edd579b68f6f4c0e
Yasen Georgiev <yasen@tavex.eu>
2024-07-02 01:21:52 +0300

    Add labels for user roles.

--- src/main/java/bg/nbuteam4/myschool/enums/Role.java
+++ src/main/java/bg/nbuteam4/myschool/enums/Role.java
@@ -3,11 +3,21 @@ package bg.nbuteam4.myschool.enums;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role  implements GrantedAuthority {
-    ADMIN,
-    DIRECTOR,
-    TEACHER,
-    STUDENT,
-    PARENT;
+    ADMIN("Администратор"),
+    DIRECTOR("Директор"),
+    TEACHER("Учител"),
+    STUDENT("Ученик"),
+    PARENT("Родител");
+
+    private final String label;
+
+    Role(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 
     @Override
     public String getAuthority() {
